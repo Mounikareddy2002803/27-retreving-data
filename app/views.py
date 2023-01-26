a359ed7433229b4842b9cc1f3652e5103cd68ac2
Mounikareddy2002803 <mounikareddy2002803@gmail.com>
2023-01-26 11:44:23 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
 from app.models import *
+from django.db.models.functions import Length
 
 # Create your views here.
 
 def display_topics(request):
     QST=Topic.objects.all()
+    QST=Topic.objects.filter(topic_name='Cricket')
     d={'topics':QST}
     return render(request,'display_topics.html',d)
 
 
 def display_webpages(request):
     QSW=Webpage.objects.all()
+    QSW=Webpage.objects.filter(topic_name='Cricket')
+    QSW=Webpage.objects.exclude(topic_name='Cricket')
+    QSW=Webpage.objects.all()[:5:]
+    QSW=Webpage.objects.all().order_by('name')
+    QSW=Webpage.objects.filter(topic_name='Kabaddi').order_by('-name')
+    QSW=Webpage.objects.all().order_by(Length('name'))
+    QSW=Webpage.objects.all().order_by(Length('name').desc())
     d={'webpages':QSW}
     return render(request,'display_webpages.html',d)   
 
